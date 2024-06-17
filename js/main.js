64797cdc9751136a987bd97e5caf7e4220d5b841
aniket-mtr <119874419+aniket-mtr@users.noreply.github.com>
2024-06-17 10:30:02 +0530

    Update main.js : changed api protocol from http to https

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-// // http://www.7timer.info/bin/api.pl?lon=113.17&lat=23.09&product=civil&output=json
+// // https://www.7timer.info/bin/api.pl?lon=113.17&lat=23.09&product=civil&output=json
 
 document.addEventListener("DOMContentLoaded", () => {
     const cityList = document.getElementById('list');
@@ -51,7 +51,7 @@ document.addEventListener("DOMContentLoaded", () => {
         const result = data.find(item => item.city === new_city_name);
         if (result) {
             (async () => {
-                let api_string = `http://www.7timer.info/bin/api.pl?lon=${result.longitude}&lat=${result.latitude}&product=civil&output=json`;
+                let api_string = `https://www.7timer.info/bin/api.pl?lon=${result.longitude}&lat=${result.latitude}&product=civil&output=json`;
                 let api_data = await getAPI(api_string);
                 if(loader)
                     {
